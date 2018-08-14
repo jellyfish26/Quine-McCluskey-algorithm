8ac7e972e7ffaa1d928ccdce717b3eb24e689f8b
jellyfish26 <jellyfish26@users.noreply.github.com>
2018-08-15 02:39:22 +0900

    [CHANGE] Improvement of input mechanism

--- main.cpp
+++ main.cpp
@@ -12,19 +12,24 @@ vector<logical_expression> logical_number;
 int Combined_number[26] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072,
                            262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432}; // A ~ Z
 
-bool Constructing_logical_expressions(string input) {
+string Constructing_logical_expressions(string input) {
     string output;
     int number = 0;
-    if (input.size() != Number_of_variables) return false;
+    if (input.size() != Number_of_variables) return "DON'T INPUT (The number of variables is different.)";
     for (int i = 0; i < input.size(); i++) {
         if (input[i] == '0' || input[i] == '1') {
             output.append(1, input[i]);
             if (input[i] == '1') number += Combined_number[i];
-        } else return false;
+        } else return "DON'T INPUT (Please enter a binary number.)";
     }
+
+    for (int i = 0; i < logical_number.size(); i++) {
+        if(logical_number[i].number == number) return "DON'T INPUT (It has already been entered.)";
+    }
+
     logical_number.push_back({output, number});
 
-    return true;
+    return "INPUT COMPLETE";
 }
 
 void debug() {
@@ -40,7 +45,7 @@ int main() {
         cout << "Please enter a logical expression. (A ~ " << (char)('A' + Number_of_variables - 1) << ")" << endl;
         string temp;
         cin >> temp;
-        if (!Constructing_logical_expressions(temp)) cout << "DON'T INPUT" << endl;
+        cout << Constructing_logical_expressions(temp) << endl;
         cout << "Add logical expression? (y / n)";
         char choice;
         cin >> choice;
